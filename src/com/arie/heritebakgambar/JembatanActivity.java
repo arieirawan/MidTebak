76ef46180b0de1d607d1320a2915b764daf2fd92
arieirawan <arie.irawan.syukran@gmail.com>
2014-12-25 17:32:08 +0700

    tugas mobile

---
+++ src/com/arie/heritebakgambar/JembatanActivity.java
@@ -0,0 +1,59 @@
+package com.arie.heritebakgambar;
+
+import com.arie.heritebakgambar.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class JembatanActivity extends Activity {
+
+	EditText jawabanTxt;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_jembatan);
+		
+		jawabanTxt = (EditText)findViewById(R.id.editText1);
+		jawabanTxt.setFilters(new InputFilter[] {new InputFilter.AllCaps()});
+	}
+	
+	public void cekJawaban(View v){
+		String  jawabanBenar = "JEMBATAN LAYANG";
+		String jawabanUser = jawabanTxt.getText().toString();
+		if (jawabanUser.equals(jawabanBenar)){			
+			Toast.makeText(getApplicationContext(), "B E N A R", Toast.LENGTH_LONG).show();
+			Intent i = new Intent(getApplicationContext(),JuaraActivity.class);
+			startActivity(i);
+		}
+		else{
+			Toast.makeText(getApplicationContext(), "S A L A H!", Toast.LENGTH_LONG).show();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.jembatan, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
